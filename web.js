c8b75822939f24ecef51f196cbae29d4d30cfc89
George Ivanov <george.ivanov@hotmail.com>
2013-08-03 04:02:05 +0000

    reading from index.html

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
-var express = require('express');
+var express = require('express')
+    fs = require('fs');
+
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+   fs.readFile('index.html', function(err, data) {
+       var contents = data;
+       console.log(contents);
+       response.send(contents);
+  });
 });
 
 var port = process.env.PORT || 5000;
